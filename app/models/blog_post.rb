fa793b81d90ff87d59bed5330827cb45ad859df7
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2024-04-24 07:07:29 -0700

    Added published_at with functionality

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,22 @@
 class BlogPost < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
+
+    scope :sorted, -> { order(published_at: :desc, created_at: :desc)}
+    scope :draft, -> { where(published_at: nil)}
+    scope :published, -> { where("published_at <= ?", Time.current) }
+    scope :scheduled, -> { where("published_at > ?", Time.current) }
+    # Ex:- scope :active, -> {where(:active => true)}
+
+    def draft?
+        published_at.nil?
+    end
+
+    def published?
+        published_at? && published_at <= Time.current
+    end
+
+    def scheduled?
+        published_at? && published_at > Time.current
+    end
 end
