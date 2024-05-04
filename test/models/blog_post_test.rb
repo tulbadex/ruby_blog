210086bffea88fa0cbd0f065d8bbe4fb6971a865
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2024-05-04 00:41:03 -0700

    add test for blog post helper method

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,52 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for draft blog post" do
+    # binding.irb
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? returns false for published blog post" do
+    refute published_blog_post.draft?
+  end
+
+  test "draft? returns false for scheduled blog post" do
+    refute scheduled_blog_post.draft?
+  end
+
+  test "published? returns true for published blog post" do
+    assert published_blog_post.published?
+  end
+
+  test "published? returns false for published blog post" do
+    refute draft_blog_post.published?
+  end
+
+  test "published? returns false for scheduled blog post" do
+    refute scheduled_blog_post.published?
+  end
+
+  test "scheduled? returns true for scheduled blog post" do
+    assert scheduled_blog_post.scheduled?
+  end
+
+  test "scheduled? returns false for scheduled blog post" do
+    refute draft_blog_post.scheduled?
+  end
+
+  test "scheduled? returns false for published blog post" do
+    refute published_blog_post.scheduled?
+  end
+
+  def draft_blog_post
+      BlogPost.new(published_at: nil)
+  end
+
+  def published_blog_post
+    BlogPost.new(published_at: 1.year.ago)
+  end
+
+  def scheduled_blog_post
+    BlogPost.new(published_at: 1.year.from_now)
+  end
 end
